d48ba659784f7abf78cac088d2b9e7029eae119a
bcarcamo247 <bcarcamo247@gmail.com>
2026-05-13 10:11:53 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+    return <p>Select an episode to learn more.</p>;
+  }
+  
+    return (
+      <section>
+        <h2>{selectedEpisode.title} #{selectedEpisode.id}</h2>
+        <p>{selectedEpisode.description}</p>
+      </section>
+    );
+  }
+
+  function EpisodeList() {
+    return (
+       <ul>
+          {episodes.map((episode) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+    
+    );
+  }
+
+    return (
+      <>
+        <h1>Dark Echoes</h1>  
+        <EpisodeList />
+        <EpisodeDetails />
+      </>
+    );
 }
